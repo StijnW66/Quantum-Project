015433d3e4af3ae67f881f6b63df24a911392cef
Stijn van de Water <S.vandeWater-1@student.tudelft.nl>
2022-01-11 11:52:02 +0100

    Add optimized version of adder circuit

--- Test/test_qiskit.py
+++ Test/test_qiskit.py
@@ -6,7 +6,7 @@ sys.path.append(".")
 import pytest
 import math
 from src.quantum.qi_runner import setup_QI, execute_circuit, print_results
-from src.quantum.quantum_gates import adder
+from src.quantum.quantum_gates import adder, adder_optimized, parse_num
 
 from quantuminspire.credentials import enable_account
 from qiskit.circuit.library import QFT
@@ -34,26 +34,10 @@ def test_entagle():
         assert math.isclose(val, 0.5, abs_tol=0.1)
 
 
-def parse_numbers(num1, num2, size):
-    # convert numbers to binary and put in a list.
-    list_num1 = [int(x) for x in bin(num1)[2:]]
-    list_num2 = [int(x) for x in bin(num2)[2:]]
-
-    # pad list with 0's
-    while (len(list_num1) < size):
-        list_num1.insert(0, 0)
-    while (len(list_num2) < size):
-        list_num2.insert(0, 0)
-
-    # reverse the second list. Due to the fourier transforms do_swaps the second number is swapped twice (which is okay),
-    # but the first number is only swapped once. However since the lists are used in reverse order later on we need to reverse the 2nd list to correct.
-    list_num2 = list_num2[::-1]
-    print(list_num1)
-    print(list_num2)
-    return list_num1, list_num2
 
 
-def create_adder_circuit(num1, num2, size):
+
+def create_adder_circuit(num1, num2, size, optimize):
     # create quantum circuit to hold value a.
     q = QuantumRegister(int(size))
     b = ClassicalRegister(size*2)
@@ -64,7 +48,8 @@ def create_adder_circuit(num1, num2, size):
     qft = QuantumCircuit(qftq)
 
     # convert the numbers to binary and set the corresponding bits to |1>
-    list_num1, list_num2 = parse_numbers(num1, num2, size)
+    list_num1 = parse_num(num1, size)
+    list_num2 = parse_num(num2, size)[::-1]
     for i in range(len(list_num1)):
         if (list_num1[i]):
             circuit.x(q[i])
@@ -78,23 +63,26 @@ def create_adder_circuit(num1, num2, size):
     circuit += qft
 
     # Apply the adder to the circuit
-    adder(circuit)
+    if (optimize):
+        adder_optimized(circuit, num1)
+    else:
+        adder(circuit)
 
     # Add inverse quantum fourier transform to bottom qubits
     iqft = QuantumCircuit(qftq)
     iqft.append(QFT(num_qubits=int(size), approximation_degree=0, do_swaps=True, inverse=True, insert_barriers=False, name='iqft'), qftq)
     circuit += iqft
 
-    # print(circuit.draw())
     return circuit
 
-def assert_add_nums(num1, num2):
+def assert_add_nums(num1, num2, optimize):
     # Calculate the sum to determine the amount of bits needed.
     sum = num1+num2
     size = len((bin(sum))) - 2 # subtract 2 since string starts with 0b
 
     # Create the circuit.
-    circuit = create_adder_circuit(num1, num2, size)
+    circuit = create_adder_circuit(num1, num2, size, optimize)
+
 
     # Execute circuit on quantuminspire and check if the correct  value is obtained.
     qi_result = execute_circuit(circuit, 3)
@@ -106,14 +94,15 @@ def assert_add_nums(num1, num2):
 def test_adder():
     # any sum should work as long as it takes half of maximum qubits.
 
-    assert_add_nums(11, 14)
-    assert_add_nums(110, 143)
-    assert_add_nums(99, 19)
-    assert_add_nums(194, 171)
-    assert_add_nums(1, 124)
-    assert_add_nums(0, 162)
-    assert_add_nums(82, 0)
+    assert_add_nums(11, 14, False)
+    assert_add_nums(110, 143, True)
+    assert_add_nums(99, 19, False)
+    assert_add_nums(194, 171, True)
+    assert_add_nums(1, 124, False)
+    assert_add_nums(0, 162, True)
+    assert_add_nums(82, 0, False)
 
-    # This test takes 8 minutes to run.
-    #assert_add_nums(5254, 2083)
+    # This test takes 8 minutes to run and 91 gates for normal adder,
+    # The optimized adder takes 6 minutes and 33 gates
+    # assert_add_nums(5254, 2083)
 
--- src/quantum/quantum_gates.py
+++ src/quantum/quantum_gates.py
@@ -10,8 +10,31 @@ def adder(circuit):
     qubit_register = circuit.qubits
     size = len(qubit_register)
 
+    # apply the cp gates
     for i in range(int(size/2)):
         for j in range(int(size/2) - i):
             circuit.cp((2*3.14* 1) /2**(j+1), qubit_register[j + i], qubit_register[i + int(size/2)])
+
+
+# parse a number to binary
+def parse_num(num, size):
+    list_num = [int(x) for x in bin(num)[2:]]
+    while (len(list_num) < size):
+        list_num.insert(0, 0)
+    return list_num
+
+# To optimize further let num be the number with the least number of bits set to 1
+def adder_optimized(circuit, num):
+    qubit_register = circuit.qubits
+    size = len(qubit_register)
+
+    # parse the number to binary.
+    list_num = parse_num(num, size/2)
+
+    # apply the cp gates
+    for i in range(int(size/2)):
+        for j in range(int(size/2) - i):
+            if (list_num[(j+i)]):
+                circuit.cp((2*3.14* 1) /2**(j+1), qubit_register[(j+i)], qubit_register[i + int(size/2)])
 
 
