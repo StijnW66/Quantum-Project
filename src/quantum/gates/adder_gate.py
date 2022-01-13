753325e1a8dafdd06be2a2e8f1cea393dbc1a248
Stijn van de Water <S.vandeWater-1@student.tudelft.nl>
2022-01-13 10:56:41 +0100

    Delete unused adders

--- src/quantum/gates/adder_gate.py
+++ src/quantum/gates/adder_gate.py
@@ -1,6 +1,13 @@
 from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
 import math
 
+# parse a number to binary
+def parse_num(num, size):
+    list_num = [int(x) for x in bin(num)[2:]]
+    while (len(list_num) < size):
+        list_num.insert(0, 0)
+    return list_num
+
 # This is the most efficient adder which can add up to 26 bit numbers.
 def adder_reduced(num, size):
     qubit_register = QuantumRegister(size)
@@ -17,34 +24,3 @@ def adder_reduced(num, size):
         circuit.p(angle_sum, qubit_register[i])
 
     return circuit
-
-
-def adder(circuit):
-    qubit_register = circuit.qubits
-    size = len(qubit_register)
-
-    # apply the cp gates
-    for i in range(int(size/2)):
-        for j in range(int(size/2) - i):
-            circuit.cp((2*3.14* 1) /2**(j+1), qubit_register[j + i], qubit_register[i + int(size/2)])
-
-
-# parse a number to binary
-def parse_num(num, size):
-    list_num = [int(x) for x in bin(num)[2:]]
-    while (len(list_num) < size):
-        list_num.insert(0, 0)
-    return list_num
-
-def adder_optimized(circuit, num):
-    qubit_register = circuit.qubits
-    size = len(qubit_register)
-
-    # parse the number to binary.
-    list_num = parse_num(num, size/2)
-
-    # apply the cp gates
-    for i in range(int(size/2)):
-        for j in range(int(size/2) - i):
-            if (list_num[(j+i)]):
-                circuit.cp((2*3.14* 1) /2**(j+1), qubit_register[(j+i)], qubit_register[i + int(size/2)])
