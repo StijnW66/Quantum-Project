c395330be83c6a4f1c7ee99a5a72581855328e6d
Andrzej Rubio Bizcaino <a.rubiobizcaino@student.tudelft.nl>
2022-01-13 23:39:20 +0100

    Add name to circuit, change gates for better readability

--- src/quantum/gates/controlled_multiplier_gate.py
+++ src/quantum/gates/controlled_multiplier_gate.py
@@ -10,22 +10,25 @@ def controlled_multiplier_gate(x_size, b_size, a, N):
     c1 = QuantumRegister(1, 'c')
     x = QuantumRegister(x_size, 'x')
     b = QuantumRegister(b_size, 'b')
-    c2 = QuantumRegister(1, 'mod_add_c')
+    c2 = QuantumRegister(1, 'mod_add_0')
 
     cr = QuantumCircuit(c1, x, b, c2)
 
     # Create QFT gate and apply to 'b' qubits
-    qft_b = QFT(b_size)
+    qft_b = QFT(num_qubits=b_size, approximation_degree=0, do_swaps=True, inverse=False, insert_barriers=False,
+                name='qft')
     cr.append(qft_b.to_instruction(), b)
 
     # Create Modular Adder Gates, set control to c1 and x[i] qubits and add to circuit
     for i in range(x_size):
-        temp_a = 2 ** i * a
+        temp_a = (2 ** i) * a
         temp_modular_adder = modular_adder(temp_a, N, b_size)
+        temp_modular_adder.name = "mod_add_" + str(2) + "^" + str(i) + "a_N"
         cr.append(temp_modular_adder, [c1[0], x[i]] + b[0:b_size] + [c2[0]])
 
     # Create QFT^(-1) gate and apply to 'b' qubits
-    inv_qft_b = QFT(b_size).inverse()
+    inv_qft_b = QFT(num_qubits=b_size, approximation_degree=0, do_swaps=True, inverse=True, insert_barriers=False,
+                    name='iqft')
 
     cr.append(inv_qft_b.to_instruction(), b)
 
@@ -67,15 +70,15 @@ def c_U_a_gate(size, a, N):
 
     # adding controlled multiplier gate
     c_mult_a_gate = controlled_multiplier_gate(size, size, a, N).to_gate()
-    circuit.append(c_mult_a_gate, range(2*size+2))
+    circuit.append(c_mult_a_gate, range(2 * size + 2))
 
     # adding controlled swap register gate
     c_swap_gate = c_swap_register(size).to_gate()
-    circuit.append(c_swap_gate, range(2*size+1))
+    circuit.append(c_swap_gate, range(2 * size + 1))
 
     # adding inverse controlled multiplier gate
-    c_mult_a_inverse_gate = controlled_multiplier_gate(size, size, modinv(a,N), N).inverse().to_gate()
-    circuit.append(c_mult_a_inverse_gate, range(2*size+1))
+    c_mult_a_inverse_gate = controlled_multiplier_gate(size, size, modinv(a, N), N).inverse().to_gate()
+    circuit.append(c_mult_a_inverse_gate, range(2 * size + 1))
 
     return circuit
 
@@ -87,6 +90,7 @@ def egcd(a, b):
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
+
 
 def modinv(a, m):
     g, x, y = egcd(a, m)
