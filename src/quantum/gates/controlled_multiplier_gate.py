8a5ca04b71f0588598d6d835b16272b78896d03b
FL0RI5 <cl3mentfake@gmail.com>
2022-01-13 10:18:45 +0100

    added comments to swap functions

--- src/quantum/gates/controlled_multiplier_gate.py
+++ src/quantum/gates/controlled_multiplier_gate.py
@@ -32,6 +32,7 @@ def controlled_multiplier_gate(x_size, b_size, a, N):
 
 
 def swap_reg(size):
+    # swaps registers x and b
     x = QuantumRegister(size, 'x')
     b = QuantumRegister(size, 'b')
     circuit = QuantumCircuit(x, b)
@@ -40,12 +41,14 @@ def swap_reg(size):
 
 
 def c_swap_register(size):
+    # controlled SWAP of registers x and b
     c = QuantumRegister(1, 'c')
     x = QuantumRegister(size, 'x')
     b = QuantumRegister(size, 'b')
 
     circuit = QuantumCircuit(c, x, b)
 
+    # Create controlled SWAP gate
     c_swap_gate = swap_reg(size).to_gate(label="SWAP").control(1)
     circuit.append(c_swap_gate, range(2 * size + 1))
 
