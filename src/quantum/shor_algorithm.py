363a690e4b112788422ac02fd80eee2fe43cde5f
Andrzej Rubio Bizcaino <a.rubiobizcaino@student.tudelft.nl>
2022-01-21 21:52:47 +0100

    Update Shor's Algorithm pre- and post-processing and update requirements.txt

--- src/quantum/shor_algorithm.py
+++ src/quantum/shor_algorithm.py
@@ -1,47 +1,83 @@
+import sys
+
 import numpy as np
 from math import gcd
-from qi_runner import setup_QI
 
-qi_backend = None
+shor_attempt_number = 0
+chosen_numbers = set()
 
 
-def find_period(a, N, qi_backend):
+def find_period(a, N):
     return 1
 
 
 def shor_algorithm(N):
-    assert N is int
-    assert N > 1
+    # Assert that passed N is valid a number
+    assert type(N) is int, "The given number is not an integer!"
+    assert N > 1, "The given number has to be greater than 1"
+
+    # Set global variables and print message to indicate start of the Shor's Algorithm
+    global shor_attempt_number
+    global chosen_numbers
+    shor_attempt_number += 1
+    print("Shor's Algorithm for " + str(N) + ": Attempt " + str(shor_attempt_number))
 
     # Small number check
-    if N < 3:
+    if N <= 3:
+        print(f"\tThe given number {N} is a prime. Terminating the algorithm.\n")
         return 1, N
 
-    # Draw random integer 'a' satisfying:  1 < a < N
-    a = np.randomint(2, N - 1)
+    # Check if N = p^q
+    if shor_attempt_number == 1:
+        for i in range(2, int(np.sqrt(N)) + 1, 1):
+            power = 0
+            temp = 1
+            while temp < N:
+                temp = temp * i
+                power += 1
+            if temp == N:
+                print(f"\tThe given number {N} can be expressed as {i}^{power}. Terminating the algorithm.\n")
+                return i, int(N / i)
+
+    # Check if based on past attempts number is prime. Preventing infinite run
+    if len(chosen_numbers) >= N - 2:
+        print(f"\t Based on past attempts, it seems that given number {N} is prime. Terminating the algorithm.\n")
+        return 1, N
+
+    # Draw random integer 'a' such that:  1 < a < N
+    a = np.random.randint(2, N)
+    while a in chosen_numbers:
+        a = np.random.randint(2, N - 1)
+    chosen_numbers.add(a)
+    print("\tChosen a: " + str(a))
     K = gcd(a, N)
 
     # Check for trivial solution
     if K != 1:
-        return K, N / K
-
-    # Setting-up connection to QI to prevent multiple authentication
-    global qi_backend
-    if qi_backend is None:
-        qi_backend = setup_QI()
+        print("\tFound trivial solution as randomly chosen 'a' shares divisor with 'N'")
+        return K, int(N / K)
 
     # Quantum Subroutine: Find Period of f(x) = a ** x mod N == 1
-    r = find_period(a, N, qi_backend)
+    # Note: The subroutine of shor's algorithm is set up to run locally.
+    r = sys.maxsize
+    while r == sys.maxsize:
+        r = find_period(a, N)
+    print("\tFound period 'r': " + str(r))
 
     # Check if a ** (r / 2) is integer
     if r % 2 == 1:
-        shor_algorithm(N)
+        print("\tPeriod is not even, repeating the algorithm.\n")
+        return shor_algorithm(N)
+    else:
+        print("\tPeriod is even, proceeding with algorithm.")
 
-    x = (a ** (r / 2)) % N
+    # Determine 'middle point' 'x'
+    x = int((a ** (r / 2)) % N)
 
     # Check if x + 1 is not multiple of N (as then gcd(x + 1, N) = N, which is not solving the problem)
     if (x + 1) % N == 0:
-        shor_algorithm(N)
+        print(f"\t{a}^({int(r / 2)}) mod {N} == -1, repeating the algorithm.\n")
+        return shor_algorithm(N)
 
     # Else return factors of N
     return gcd(x + 1, N), gcd(x - 1, N)
